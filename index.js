cea7934d1e04cfbe8dee19dabfd878c4179b26cf
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-03-16 12:37:14 -0400

    add random number generation

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const shajs = require('sha.js');
+const randomBinary = require('random-binary');
 
+const MESSAGE = '10001';
+const NUMBER_OF_BITS = 5;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('sha256 for networksecurity: ' + shajs('sha256').update('networksecurity').digest('hex'));
+  res.send({
+    sha256: shajs('sha256').update(MESSAGE).digest('hex'),
+    randomBinary: randomBinary(NUMBER_OF_BITS)
+  });
 })
 
 app.listen('8080', () => {
