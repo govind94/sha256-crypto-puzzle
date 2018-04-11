c13b110f1fd50c6d2029f698ae5f3670e2e00975
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-04-10 20:16:04 -0400

    change #bytes

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 const express = require('express');
 const shajs = require('sha.js');
 const randomBinary = require('random-binary');
+const hexToBinary = require('hex-to-binary');
 
-const MESSAGE = '10001';
-const NUMBER_OF_BITS = 5;
-const P_BYTES = 3 // 12? (4 * 3)
 const app = express();
+const NUMBER_OF_BITS_FOR_M = 1000;
 
 app.use((req, res, next) => {
-  let messageHash = shajs('sha256').update(MESSAGE).digest('hex');
-  let messageLastP = messageHash.substring(messageHash.length-P_BYTES);
-  while(true) {
-    let generateRandom = randomBinary(NUMBER_OF_BITS);
-    let guessNumber = shajs('sha256').update(generateRandom).digest('hex');
-    let guessLastP = guessNumber.substring(guessNumber.length-P_BYTES);
-    console.log(generateRandom , guessLastP);
-    if(guessLastP === messageLastP) {
-      console.log("Fiiiinnnnaaaallllllyyyyy!");
-      break;
+  // Different values of B: 1, 2, 4, 8, 16 bytes
+  // loop for B goes here
+  let B, arr = [];
+  for(B = 1; B < 3; B *= 2) {
+    let NUMBER_OF_BITS_FOR_P = 8 * B;
+    // Number of trials: 1000 for B = 1, 2, 4; 10 or 20 for B = 8; 1 for B = 16
+    // loop for no. of trials goes here
+    let sum = 0;
+    for(let i = 0; i < 1; i++) {
+      let P = randomBinary(NUMBER_OF_BITS_FOR_P);
+      let count;
+      for(count = 0; ; count++) {
+        let M = randomBinary(NUMBER_OF_BITS_FOR_M);
+        let shaMHex = shajs('sha256').update(M).digest('hex');
+        let shaMBinary = hexToBinary(shaMHex);
+        let shaMBinaryLastB = shaMBinary.substring(shaMBinary.length-NUMBER_OF_BITS_FOR_P);
+        console.log(i, P, shaMBinaryLastB);
+        if(P === shaMBinaryLastB) break;
+      }
+      sum += count;
     }
+    arr.push(sum);
   }
+  console.log("Fiiiinnnnaaaallllllyyyyy!");
+  res.locals.arr = arr;
   next();
-})
+});
 
 app.get('/', (req, res) => {
   // res.send({
   //   sha256: shajs('sha256').update(MESSAGE).digest('hex'),
   //   randomBinary: randomBinary(NUMBER_OF_BITS)
   // });
-
-  res.send("If I show up, it's a good thing!");
-})
+  res.send({
+    message: "If I show up, it's a good thing!",
+    array: res.locals.arr
+  });
+});
 
 app.listen('8080', () => {
   console.log("Server running...");
-})
+});
