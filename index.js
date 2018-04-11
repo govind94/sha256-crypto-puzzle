4005f8acb87289a95eab6c1a62d034c099fd4f70
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-04-10 20:25:52 -0400

    package.json

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ const app = express();
 const NUMBER_OF_BITS_FOR_M = 1000;
 
 app.use((req, res, next) => {
-  // Different values of B: 1, 2, 4, 8, 16 bytes
-  // loop for B goes here
   let B, arr = [];
   for(B = 1; B < 3; B *= 2) {
     let NUMBER_OF_BITS_FOR_P = 8 * B;
-    // Number of trials: 1000 for B = 1, 2, 4; 10 or 20 for B = 8; 1 for B = 16
-    // loop for no. of trials goes here
     let sum = 0;
     for(let i = 0; i < 1; i++) {
       let P = randomBinary(NUMBER_OF_BITS_FOR_P);
@@ -36,10 +32,6 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send({
-  //   sha256: shajs('sha256').update(MESSAGE).digest('hex'),
-  //   randomBinary: randomBinary(NUMBER_OF_BITS)
-  // });
   res.send({
     message: "If I show up, it's a good thing!",
     array: res.locals.arr
