9aeeab275e2fefb71bb460b33e3732e10d1ed508
Govindan Vedanarayanan <vamsivedanarayanan@gmail.com>
2018-04-15 10:30:25 -0400

    final

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var NUMBER_OF_BITS_FOR_M;
 
 app.use((req, res, next) => {
   let B, arr = []; // 'arr' stores avg count for each 'B'
-  for(B = 4; B < 5; B++) {
+  for(B = 1; B < 5; B++) {
     console.log(`\n#byte(s) = ${B}`);
     let NUMBER_OF_BITS_FOR_P = 8 * B; // get #bits
     NUMBER_OF_BITS_FOR_M = 256;
     let sum = 0;
     console.time('totalTimeTaken');  // record total time
-    for(let i = 1; i <= 3; i++) { // #trials
+    for(let i = 1; i <= 1000; i++) { // #trials
       let P = randomBinary(NUMBER_OF_BITS_FOR_P);
       let count;
       console.time('timeTakenPerTrial'); // record time per trial
@@ -27,17 +27,17 @@ app.use((req, res, next) => {
         if(P === shaMBinaryLastB) break;
         if(count % 10000000 === 0) { // change #bits of M after every 10000000 attempt
           NUMBER_OF_BITS_FOR_M = Math.floor(Math.random() * 256 + 1);
-          console.log(NUMBER_OF_BITS_FOR_M);
+          console.log(`Number of bits for M changed to: ${NUMBER_OF_BITS_FOR_M}`);
         }
       }
       console.log(i, NUMBER_OF_BITS_FOR_M, P, count);
       console.timeEnd('timeTakenPerTrial'); // save time per trial
-      sum += count/3;
+      sum += count/1000;
     }
     arr.push(sum);
+    console.timeEnd('totalTimeTaken'); // save total time
   }
   console.log("\nAverage count: ", arr);
-  console.timeEnd('totalTimeTaken'); // save total time
   res.locals.arr = arr;
   next();
 });
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.send({
     message: "If I show up, it's a good thing!",
-    array: res.locals.arr
+    countArray: res.locals.arr
   });
 });
 
